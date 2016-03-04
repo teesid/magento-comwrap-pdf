288701ef1552ef8256e0c0c09c85617fe23637aa
Teesid Korsrilabutr <teesid@instasim.com>
2016-03-04 17:52:08 +0700

    Pre-generated all font data in ttfontdata/.

---
+++ app/code/local/Comwrap/Pdf/lib/MPDF56/ttfontdata/linuxlibertinec.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='LinLibertineC';
+$type='TTF';
+$desc=array (
+  'Ascent' => 894,
+  'Descent' => -237,
+  'CapHeight' => 894,
+  'Flags' => 4,
+  'FontBBox' => '[-446 -298 6171 894]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 500,
+);
+$up=-117;
+$ut=39;
+$ttffile='/var/www/dev.instasim.com/.modman/magento-comwrap-pdf/app/code/local/Comwrap/Pdf/lib/MPDF56/ttfonts/LinLibertineCondensed.ttf';
+$TTCfontID='0';
+$originalsize=630340;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='linuxlibertinec';
+$panose=' 1 5 2 0 5 3 0 0 0 0 0 0';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>
